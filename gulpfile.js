3f928cdfa0bf759ef448445f2edd1246bc1ba5e9
Kejda <kejdaorgocka18@gmail.com>
2019-10-25 16:46:21 +0200

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function() {
+  console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles.styles.css').pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+
+  watch('./app/index.html', function() {
+    gulp.series('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.series('styles');
+  });
+});
